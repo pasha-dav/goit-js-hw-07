61a2b4aacda83293b01326c3365170cf483a5f7c
Pasha <davedukpasha25cv@gmail.com>
2026-09-17 20:18:43 +0300

    7

---
+++ js/task-4.js
@@ -0,0 +1,20 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const email = loginForm.elements.email.value.trim();
+  const password = loginForm.elements.password.value.trim();
+
+  if (email === '' || password === '') {
+    return alert('All form fields must be filled in');
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  loginForm.reset();
+});
